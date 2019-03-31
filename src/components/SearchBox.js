3fe3ddc31c06f053e9060e04b006dd99085dd3ca
Wade Oghenejabor <oghenejaborwade@gmail.com>
2019-04-01 00:55:25 +0100

    initial commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = ({ searchfield, searchChange }) => {
+	return (
+		<div className='pa2'>
+			<input className='pa3 ba b--yellow bg--yellow'
+				type='search' 
+				placeholder='search robots' 
+				onChange={searchChange}
+			/>
+		</div>	
+	);
+}
+export default SearchBox; 
---
+++ src/components/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Ik ThankGod',
+		username: 'Richboy',
+		email: 'Richboy@new.co'
+	},
+	{
+		id: 2,
+		name: 'Igwe Favour',
+		username: 'Yumpkin',
+		email: 'Daddysgirl@new.co'
+	},
+	{
+		id: 3,
+		name: 'Udih Ovie',
+		username: 'Touchlight',
+		email: 'Rapking@new.co'
+	},
+	{
+		id: 4,
+		name: 'Micheal Imara',
+		username: 'Mimara',
+		email: 'Mimaraconcept@new.co'
+	},
+	{
+		id: 5,
+		name: 'Samuel Ehuwa',
+		username: 'Skart',
+		email: 'Singer@new.co'
+	},
+	{
+		id: 6,
+		name: 'Ufouma Pokoh',
+		username: 'Magic',
+		email: 'fingers@new.co'
+	},
+	{
+		id: 7,
+		name: 'Bassey Precious',
+		username: 'Pee-Drums',
+		email: 'drummerboy@new.co'
+	},
+	{
+		id: 8,
+		name: 'Wajey Gee',
+		username: 'Yellow',
+		email: 'pawpaw@new.co'
+	},
+	{
+		id: 9,
+		name: 'Wade Courage',
+		username: 'South',
+		email: 'south@new.com'
+	},
+	{
+		id: 10,
+		name: 'Ozedikus Nwanne',
+		username: 'Nwanne',
+		email: 'gunnerato@new.com'
+	}
+];
