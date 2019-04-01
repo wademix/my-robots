3d829c23c2298f803cf1b0e95f4a76ec54601bc3
Wade Oghenejabor <oghenejaborwade@gmail.com>
2019-04-01 12:23:26 +0100

    Updates

---
+++ precache-manifest.7e747b70c740d094d570a8667c5e5db8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/wademix/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "931652d1e034df8e119c",
+    "url": "/wademix/robofriends.git/static/js/runtime~main.d6ba2ed6.js"
+  },
+  {
+    "revision": "393aedbaed87ba2aca14",
+    "url": "/wademix/robofriends.git/static/js/main.cb54550f.chunk.js"
+  },
+  {
+    "revision": "6318217c2d32737e8d55",
+    "url": "/wademix/robofriends.git/static/js/2.8cad79b2.chunk.js"
+  },
+  {
+    "revision": "393aedbaed87ba2aca14",
+    "url": "/wademix/robofriends.git/static/css/main.522d0ee2.chunk.css"
+  },
+  {
+    "revision": "6318217c2d32737e8d55",
+    "url": "/wademix/robofriends.git/static/css/2.87223604.chunk.css"
+  },
+  {
+    "revision": "87bfdf4c6f74912a7507258e1c48905e",
+    "url": "/wademix/robofriends.git/index.html"
+  }
+];
